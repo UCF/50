7d7b91c8e5fed5b8ae47ead51298d12bfab2f3ca
Doug Beck <doug@douglasbeck.com>
2012-01-10 22:28:21 -0500

    small bug, closing body correctly

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 				<?=get_menu('footer-menu', 'menu horizontal', 'footer-menu')?>
 			</div>
 		</div><!-- #blueprint-container -->
-	</body>
 	<!--[if IE]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 	<?="\n".footer_()."\n"?>
+	</body>
 </html>
