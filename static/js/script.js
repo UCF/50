d86937207c1bca4bf4d357820b4ef49ce6430e9d
Chris Conover <clcono@gmail.com>
2012-01-31 14:14:40 -0500

    IE7 fixes

--- static/js/script.js
+++ static/js/script.js
@@ -10,12 +10,22 @@ if (typeof jQuery != 'undefined'){
 		
 		/* Theme Specific Code Here */
 		(function() {
+			var ie7 = false;
+
+			if($.browser.msie && $.browser.version == '7.0') {
+				ie7 = true
+			}
+
 			// Front Page Image Scaling
 			$(window).resize(function() {
 				var front_page_image = $('#front-page #feature-wrap img'),
 					body_width = $(window).width(),
 					body_height = $(window).height();
 				
+				if(ie7) {
+					body_height = body_height * 1.5; // why? who knows
+				}
+
 				var aspect_ratio = (front_page_image.width() / front_page_image.height());
 
 				var new_width = Math.round(body_height * aspect_ratio);
