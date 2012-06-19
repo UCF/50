3bf568041aaaeda040990ab42531c351f008eecc
Chris Conover <clcono@gmail.com>
2012-06-19 14:56:04 -0400

    Final iPad formatting fixes

--- static/js/script.js
+++ static/js/script.js
@@ -288,16 +288,26 @@
 
 				};
 				if(ipad) {
+					if($('#front-page').length) {
+						$('#feature-wrap img').wrap('<span />');
+					}
+					
 					function on_orientation_change() {
 						if(orientation == '0' || orientation == '180') {
 							$('#UCFHBHeader div.UCFHBWrapper').width('768px');
 							$('body').scrollLeft(0)
 							$('#blueprint-container').width('768px');
 							$('#header-wrap.front-page').width('768px');
+							if(!$('#front-page').length) {
+								$('body').css('zoom', '.65');
+							}
 						} else {
 							$('#UCFHBHeader div.UCFHBWrapper').width('974px');
 							$('#blueprint-container').width('950px');
 							$('#header-wrap.front-page').width('100%');
+							if(!$('#front-page').length) {
+								$('body').css('zoom', '1');
+							}
 						}
 					}
 					$(window).bind('orientationchange', on_orientation_change);
