b1a6df4eccb3ddc74d1b51366bdbb06242b8452a
Jo Greybill <jogreybill@gmail.com>
2012-07-10 14:39:20 -0400

    Added theme-specific functions

--- functions.php
+++ functions.php
@@ -8,4 +8,30 @@ require_once('shortcodes.php');         		# Per theme shortcodes
 
 //Add theme-specific functions here.
 
+/**
+ * Get a random post from the FrontPage post type 
+ *
+ *
+ * @author Chris Conover
+ **/
+function get_front_page_post() {
+	$front_page_posts  = get_posts(array('numberposts'=>1,'orderby'=>'rand', 'post_type'=>'frontpage'));
+	$post_thumbnail_id = get_post_thumbnail_id($front_page_posts[0]->ID);
+	$featured_image    = wp_get_attachment_image_src($post_thumbnail_id,'single-post-thumbnail');
+	return array(
+		'post'           => $front_page_posts[0],
+		'featured_image' => $featured_image);
+}
+
+/**
+ * Truncates a string based on word count
+ *
+ * @return string
+ * @author Chris Conover
+ **/
+function truncate($string, $word_count=30) {
+	$parts = explode(' ', $string, $word_count);
+	return implode(' ', array_slice($parts, 0, count($parts) - 1)).'...';
+}
+
 ?>
