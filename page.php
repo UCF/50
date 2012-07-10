b29c530bc4f29f9eb56724f5f15394d5ccded066
Jo Greybill <jogreybill@gmail.com>
2012-07-10 15:19:27 -0400

    Removed excess .span12

--- page.php
+++ page.php
@@ -1,12 +1,10 @@
 <?php disallow_direct_load('page.php');?>
 <?php get_header(); the_post();?>
 	<div class="row page-content" id="<?=$post->post_name?>">
-		<div class="span12">
-			<article>
-				<h1><?php the_title();?></h1>
-				<?php the_content();?>
-			</article>
-		</div>
+		<article>
+			<h1><?php the_title();?></h1>
+			<?php the_content();?>
+		</article>
 	</div>
 
 <?php get_footer();?>
