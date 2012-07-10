a7816a681fb026c5296d1741405a6b59216e1886
Jo Greybill <jogreybill@gmail.com>
2012-07-10 16:34:41 -0400

    Added the base for a footer

--- footer.php
+++ footer.php
@@ -1,4 +1,25 @@
-		</div><!-- .container -->
+		<div id="footer">	
+			<div class="container">
+				<div class="row">
+					<div class="span3">
+						<strong>title</strong>
+						<p>some stuff will go here</p>
+					</div>
+					<div class="span3">
+						<strong>title</strong>
+						<p>some stuff will go here</p>
+					</div>
+					<div class="span3">
+						<strong>title</strong>
+						<p>some stuff will go here</p>
+					</div>
+					<div class="span3">
+						<strong>title</strong>
+						<p>some stuff will go here</p>
+					</div>
+				</div><!-- .row -->
+			</div><!-- .container -->
+		</div><!-- #footer -->
 	</body>
 	<!--[if IE]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
