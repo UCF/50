cfc9cb820178326973279e55e5b6a7d0d22495aa
Jo Greybill <jogreybill@gmail.com>
2012-07-10 14:52:08 -0400

    Updated styles with bootstrap classes

--- page.php
+++ page.php
@@ -1,1 +1,12 @@
-<?php include_once('template-one-column.php');?>
+<?php disallow_direct_load('page.php');?>
+<?php get_header(); the_post();?>
+	<div class="row page-content" id="<?=$post->post_name?>">
+		<div class="span12">
+			<article>
+				<h1><?php the_title();?></h1>
+				<?php the_content();?>
+			</article>
+		</div>
+	</div>
+
+<?php get_footer();?>
