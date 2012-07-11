b3c72ff24013dc2bd80b3b6b949cdc35b07d584d
Jo Greybill <jogreybill@gmail.com>
2012-07-11 10:48:08 -0400

    Added footer

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php get_header();?>
-<div class="page-content" id="front-page">
-	<? extract(get_front_page_post()); ?>
-	<div id="feature-wrap">
-		<div>
-			<img src="<?=$featured_image[0]?>" />
+	<div class="page-content" id="front-page">
+		<? extract(get_front_page_post()); ?>
+		<div id="feature-wrap">
+			<div>
+				<img src="<?=$featured_image[0]?>" />
+			</div>
+		</div>
+		<div id="post-content">
+			<h2 class="sans"><?=$post->post_title?></h2>
+			<div id="post-body">
+				<?=apply_filters('the_content', $post->post_content)?>
+			</div>
 		</div>
 	</div>
-	<div id="post-content">
-		<h2 class="sans"><?=$post->post_title?></h2>
-		<div id="post-body">
-			<?=apply_filters('the_content', $post->post_content)?>
-		</div>
-	</div>
-</div>
+</div><!--.container -->
 <?php get_footer();?>
